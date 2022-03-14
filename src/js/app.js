554a0f320fdfae31c68a8c189511601b698edbb5
Zenon Marek Raubuć <m.raubuc@wp.pl>
2022-03-14 10:44:16 +0100

    Update styles

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
-const querySelector = (selector) => document.querySelector(selector);
-
-const btn = querySelector('.convertor__btn');
-const result = querySelector('.convertor__result');
-
-const addEventListener = (element, callback) =>
-  element.addEventListener('click', callback);
+// const querySelector = (selector) => document.querySelector(selector);
+//
+// const btn = querySelector('.convertor__btn');
+// const result = querySelector('.convertor__result');
+// const value = querySelector('.convertor__value');
+//
+// const addEventListener = (element, callback) =>
+//   element.addEventListener('click', callback);
